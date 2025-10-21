0f9356fa25265fd662e4c3457561805cb05b3550
vrp-winner <vorapol.saen@bumail.net>
2025-10-22 01:59:48 +0700

    Add Sprite, Animator to Player & implement animation logic in Player Controller

--- Assets/RoomKeeper/Scripts/Player/PlayerController.cs
+++ Assets/RoomKeeper/Scripts/Player/PlayerController.cs
@@ -3,14 +3,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private static readonly int AnimWalking = Animator.StringToHash("Walk");
+    private static readonly int AnimDirection = Animator.StringToHash("Direction");
+    
     [Header("Player Settings")]
     [Min(0.1f), SerializeField] private float speed = 1f;
 
+    private Animator _animator;
     private Rigidbody2D _rigidbody;
     private Vector2 _playerVelocity;
 
     private void Awake()
     {
+        _animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -20,6 +25,27 @@ public class PlayerController : MonoBehaviour
 
         if (Gamepad.current == null) return;
         _playerVelocity = Gamepad.current.leftStick.ReadValue();
+        
+        if (_playerVelocity.magnitude < 0.1f)
+        {
+            _animator.SetBool(AnimWalking, false);
+            return;
+        }
+        
+        float angle = Mathf.Atan2(_playerVelocity.y, _playerVelocity.x) * Mathf.Rad2Deg;
+        int direction = 0;
+        
+        if (angle >= 45f && angle < 135f)
+            direction = 2;
+        else if (angle >= -135f && angle < -45f)
+            direction = 0;
+        else if (angle >= -45f && angle < 45f)
+            direction = 1;
+        else
+            direction = 3;
+        
+        _animator.SetBool(AnimWalking, true);
+        _animator.SetInteger(AnimDirection, direction);
     }
 
     private void FixedUpdate()
